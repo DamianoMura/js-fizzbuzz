ee6f88c56e350d743dba974e168085089ba52780
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 16:46:34 +0200

    [add] for cycle

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ let message;
 //
 //iniziamo con un ciclo for usando il suo indice nei controlli if 
 // determiniamo cosa stamperemo utilizzando la variabile message 
+for(  x=1; x<=100; x++){
+  
+  
+}
+  
