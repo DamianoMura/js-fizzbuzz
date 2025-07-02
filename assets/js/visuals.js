6805d41623dd8f06cc9bf48d443bf6731491505e
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 23:35:00 +0200

    [add]switches functions for the lights

--- assets/js/visuals.js
+++ assets/js/visuals.js
@@ -2,17 +2,43 @@ console.log("goditi la versione interattiva");
 
 
 // funzione per cambiare i nomi delle classi con javascript per buzz
-function buzzOn(){
-let classValue= document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue;
- console.log(classValue);
-classValue=classValue.replace("off", "on");
-console.log(classValue);
+function buzzSwitch(){
+  let classValue;
+  if (document.getElementsByClassName("buzz").item(0).classList.item(1) == "off"){
+    classValue= document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue;
+    classValue=classValue.replace("off", "on");
+  }
+  else {
+    classValue= document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue;
+    classValue=classValue.replace("on", "off");
+  }
 document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue=classValue;
 }
-function fizzOn(){
-let classValue= document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue;
- console.log(classValue);
-classValue=classValue.replace("off", "on");
-console.log(classValue);
+
+// funzione per cambiare i nomi delle classi con javascript per fizz
+function fizzSwitch(){
+  let classValue;
+  if (document.getElementsByClassName("fizz").item(0).classList.item(1) == "off"){
+    classValue= document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue;
+    classValue=classValue.replace("off", "on");
+  }
+  else {
+    classValue= document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue;
+    classValue=classValue.replace("on", "off");
+  }
 document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue=classValue;
 }
+
+
+
+  
+
+
+// Create a text node:
+//const textnode = document.createTextNode("Water");
+
+// Append the text node to the "li" node:
+//node.appendChild(textnode);
+
+// Append the "li" node to the list:
+//document.getElementById("myList").appendChild(node);
