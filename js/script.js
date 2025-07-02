5531b9ae24e20edfb5a4b7a671f5441cdfbd984a
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 16:48:16 +0200

    [add] first condition

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ let message;
 //iniziamo con un ciclo for usando il suo indice nei controlli if 
 // determiniamo cosa stamperemo utilizzando la variabile message 
 for(  x=1; x<=100; x++){
-  
+  //prima condizione con l'operatore end 
+  // riconosce subito il caso specifico
+  // non compromettendo i numeri che sono divisibile solo per un numero tra i due
+  if(x%3==0 && x%5==0){
+    message="FizzBuzz";
+  }
   
 }
   
