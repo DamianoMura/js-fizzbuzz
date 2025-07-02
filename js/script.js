ce731d3a7feaa4206356ec8974c9da08550f046f
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 16:54:22 +0200

    [add] 4° condition

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-// variables
-let message;
 
-//logic
-//
-//iniziamo con un ciclo for usando il suo indice nei controlli if 
+// iniziamo con un ciclo for usando il suo indice nei controlli if 
 // determiniamo cosa stamperemo utilizzando la variabile message 
+// dichiarata dentro il for
 for(  x = 1; x <= 100; x++ ){
-  //prima condizione con l'operatore end 
+  let message;
+  // prima condizione con l'operatore end 
   // riconosce subito il caso specifico
   // non compromettendo i casi singoli di divisibilità
   if(x % 3 ==0 && x % 5 == 0){
     message="FizzBuzz";
   }
-  //i prossimi due cicli riguardano i casi singoli di divisibilità
+  // i prossimi due cicli riguardano i casi singoli di divisibilità
   else if(x % 3 == 0){
     message="Fizz";
   }
   else if(x % 5 == 0){
     message="Buzz";
   }
+  // ultimo caso, non è divisibile ne per 3 nè per 5 quindi 
+  // stampiamo il numero così com'è
+  else message=`${x}`;
+
   
 }
   
