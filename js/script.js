a8a14e0dc33177f0b2320a1bb73666265ff3fee5
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 17:04:22 +0200

    fine - code cleanup

--- js/script.js
+++ js/script.js
@@ -7,19 +7,19 @@ for(  x = 1; x <= 100; x++ ){
   // prima condizione con l'operatore end 
   // riconosce subito il caso specifico
   // non compromettendo i casi singoli di divisibilità
-  if(x % 3 ==0 && x % 5 == 0){
-    message="FizzBuzz";
+  if(x % 3 == 0 && x % 5 == 0){
+    message = "FizzBuzz";
   }
   // i prossimi due cicli riguardano i casi singoli di divisibilità
   else if(x % 3 == 0){
     message="Fizz";
   }
   else if(x % 5 == 0){
-    message="Buzz";
+    message = "Buzz";
   }
   // ultimo caso, non è divisibile ne per 3 nè per 5 quindi 
   // stampiamo il numero così com'è
-  else message=`${x}`;
+  else message = `${x}`;
 
   //esponiamo i risultati alla fine di ogni ciclo del for.
   console.log(`${message}`);
