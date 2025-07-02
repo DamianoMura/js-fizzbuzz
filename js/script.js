d20455c61b813291c7b9c231c11c83359e99d110
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 16:50:34 +0200

    [add] thinking of second and third case

--- js/script.js
+++ js/script.js
@@ -5,13 +5,15 @@ let message;
 //
 //iniziamo con un ciclo for usando il suo indice nei controlli if 
 // determiniamo cosa stamperemo utilizzando la variabile message 
-for(  x=1; x<=100; x++){
+for(  x = 1; x <= 100; x++ ){
   //prima condizione con l'operatore end 
   // riconosce subito il caso specifico
-  // non compromettendo i numeri che sono divisibile solo per un numero tra i due
-  if(x%3==0 && x%5==0){
+  // non compromettendo i casi singoli di divisibilità
+  if(x % 3 ==0 && x % 5 == 0){
     message="FizzBuzz";
   }
+  //i prossimi due cicli riguardano i casi singoli di divisibilità
+  
   
 }
   
