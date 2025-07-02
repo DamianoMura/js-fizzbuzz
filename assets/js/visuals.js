e200614eab8389622933acfad23851c2ac7aa1ab
DamianoMura <mura.damiano14@gmail.com>
2025-07-02 19:58:00 +0200

    [upd] testing css class change with js

--- assets/js/visuals.js
+++ assets/js/visuals.js
@@ -1,2 +1,18 @@
 console.log("goditi la versione interattiva");
 
+
+// funzione per cambiare i nomi delle classi con javascript per buzz
+function buzzOn(){
+let classValue= document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue;
+ console.log(classValue);
+classValue=classValue.replace("off", "on");
+console.log(classValue);
+document.getElementsByClassName("buzz").item(0).attributes.class.nodeValue=classValue;
+}
+function fizzOn(){
+let classValue= document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue;
+ console.log(classValue);
+classValue=classValue.replace("off", "on");
+console.log(classValue);
+document.getElementsByClassName("fizz").item(0).attributes.class.nodeValue=classValue;
+}
